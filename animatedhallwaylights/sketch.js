946342e7f9b95849090779e60b1d95652629eed7
scotchANDsolder <70717743+scotchANDsolder@users.noreply.github.com>
2021-03-12 11:43:12 -0500

    Update sketch.js
    Made changes to make it look like more like hallway

--- animatedhallwaylights/sketch.js
+++ animatedhallwaylights/sketch.js
@@ -13,29 +13,31 @@ function setup() {
   //noStroke();
   
   //noLoop();
+   for(let x =0; x < 10;x++){
+    noiseList[x] = 0.7*x;
+  }
   
 }
 
 function draw() {
   background(0);
 
-  for(let x =0; x < 10;x++){
-    noiseList[x] = random(5);
-  }
+ 
   let j = 0;
-  for(let i = 0; i < 400;i+=40){
+  for(let i = 0; i < 400;i+=55){
     
 
-    sw = 255- noise(noiseList[j])*255;
+    sw = 255- noise(noiseList[j])*225;
     //strokeWeight(sw);
     //strokeWeight(random(0.1,3));
     //noFill();
     fill(255,255,0,sw);
     noStroke();
+    //circle(1000,500,10);
 
     quad(750-i*1.75, 500-i,1000+i*1.75,500-i,1000+(i+30)*1.75,500-(i+30),750-(i+30)*1.75, 500-(i+30));
 
-    noiseList[j] = noiseList[j] + 0.1;
+    noiseList[j] = noiseList[j] + 0.05;
 
     j = j + 1;
   }
